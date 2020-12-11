3cb13a57e0800a1ccfb6fea766d85a4f3a87d43f
Anmol Majithia <majithiaanmol12@gmail.com>
2020-12-11 17:14:34 +0530

    Dark theme adaptation test

--- themes/index.js
+++ themes/index.js
@@ -14,8 +14,8 @@ const themes = {
   dark: {
     title_color: "fff",
     icon_color: "79ff97",
-    text_color: "9f9f9f",
-    bg_color: "151515",
+    text_color: "ffc300",
+    bg_color: "0d1117",
   },
   radical: {
     title_color: "fe428e",
